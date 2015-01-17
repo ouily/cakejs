73bc9a761e4637a463c8fc67382e78bc472803e0
Willy <willy.lenormand@gmail.com>
2015-01-17 19:25:41 +0100

    Gestion basique des routes

--- cake.js
+++ cake.js
@@ -4,26 +4,51 @@
 
     window["Cake"] = function() {
 
+        var routes = {};
+
+        /**
+         * Event fired when hash change
+         **/
+
         window.addEventListener("hashchange", function(event) {
             var hash = window.location.hash;
+
             if (hash.charAt(1) == "/") {
                 hash = hash.substr(2);
             }
             if (hash.charAt(hash.length - 1) == "/") {
                 hash = hash.substr(0, hash.length - 1);
             }
-            var tab = hash.split("/");
-            var controller = tab[0];
-            var action = tab[1];
-            var params = tab.slice(2);
 
-            window["Cake"]["controllers"][controller][action](params);
+            if (routes[hash]) {
+                window["Cake"]["controllers"][routes[hash]["controller"]][routes[hash]["action"]]();
+            } else {
+                var tab = hash.split("/");
+                var controller = tab[0];
+                var action = tab[1];
+                var params = tab.slice(2);
+
+                window["Cake"]["controllers"][controller][action](params);
+            }
 
         });
 
         return {
+
+            /**
+             * Attach controller to Cake
+             **/
+
             attachController: function(name, ctrl) {
                 window["Cake"]["controllers"][name] = ctrl;
+            },
+
+            /**
+             * Attach route to controller/action
+             **/
+
+            route: function(str, tab) {
+                routes[str] = tab;
             }
             controllers: {}
         };
