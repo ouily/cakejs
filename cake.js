dfae3aa62b8a0ddc549e9afec0a22cf01af06fdb
Willy <willy.lenormand@gmail.com>
2015-01-17 18:57:28 +0100

    Update

--- cake.js
+++ cake.js
@@ -1,32 +1,32 @@
 (function() {
-  
-  "use strict";
-  
-  window["Cake"] = function() {
 
-    window.addEventListener("hashchange", function(event) {
-      var hash = window.location.hash;
-      if(hash.charAt(1) == "/") {
-        hash = hash.substr(2);
-      }
-      if(hash.charAt(hash.length - 1) == "/") {
-        hash = hash.substr(0,hash.length -1);
-      }
-      var tab = hash.split("/");
-      var controller = tab[0];
-      var action = tab[1];
-      var params = tab[2];
+    "use strict";
 
-      window["Cake"]["controllers"][controller][action](params);
+    window["Cake"] = function() {
 
-    });
+        window.addEventListener("hashchange", function(event) {
+            var hash = window.location.hash;
+            if (hash.charAt(1) == "/") {
+                hash = hash.substr(2);
+            }
+            if (hash.charAt(hash.length - 1) == "/") {
+                hash = hash.substr(0, hash.length - 1);
+            }
+            var tab = hash.split("/");
+            var controller = tab[0];
+            var action = tab[1];
+            var params = tab.slice(2);
 
-    return {
-      attachController: function(name, ctrl) {
-        window["Cake"]["controllers"][name] = ctrl;
-      }
-      controllers: {}
-  }
-}();
+            window["Cake"]["controllers"][controller][action](params);
 
- })();
+        });
+
+        return {
+            attachController: function(name, ctrl) {
+                window["Cake"]["controllers"][name] = ctrl;
+            }
+            controllers: {}
+        };
+    }();
+
+})();
